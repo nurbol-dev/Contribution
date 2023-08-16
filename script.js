987fa02932dded947abe7b0fa89aeb6e7125edce
nurbol-dev <baevnurbol@gmail.com>
2023-08-16 16:14:33 +0600

    fs

---
+++ script.js
@@ -0,0 +1,4 @@
+const graphContainer = document.querySelector('.graph');
+const daysOfWeekContainer = document.querySelector('.days-of-week');
+const monthsContainer = document.querySelector('.months');
+const apiUrl = 'https://dpg.gg/test/calendar.json';
